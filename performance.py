526dca6cca41905d1fb61d5596675c1d73b1d7dc
phildeutsch <philipp.g.deutsch@gmail.com>
2014-01-14 16:19:59 +0100

    Update performance.py

--- performance.py
+++ performance.py
@@ -1,5 +1,12 @@
 from bbSettings import *
 from bbPerformance import *
 
-makePerformanceTable(logFileNameBT)
-
+logFileNameBT  = 'Logs/' + str(tradeBuffer)
+logFileNameBT += str(priceWindow)
+logFileNameBT += str(momFactor)
+logFileNameBT += str(midDistance)
+logFileNameBT += str(walkUp)
+logFileNameBT += str(walkDown)
+logFileNameBT += str(minTrade) + '.csv'
+                            
+makePerformanceTable(logFileName)

88b3ff7a24f5bdd36e123dbd9de12110c4152556
phildeutsch <philipp.g.deutsch@gmail.com>
2014-01-26 14:02:28 +0100

    funding.py

--- funding.py
+++ funding.py
@@ -15,3 +15,13 @@ krakenAPI = krakenex.API(key, secret)
 m, p = getData(krakenAPI, m, p, t)
 
 print('Current Value: ' + '{0:4.0f}'.format(p.value))
+action = raw_input('What do you want to do? (f)und, (w)ithdraw: ')
+if action is 'f':
+    amt = raw_input('Amount (BTC) to fund: ')
+    with open(transferFile, 'at') as txFile:
+        txFile.write(m.time + ',')
+        txFile.write(str(m.bid) + ',')
+        txFile.write(str(m.ask) + ',')
+        txFile.write(str(amt) + '\n')
+else:
+    print('I do not know what you want to do.')

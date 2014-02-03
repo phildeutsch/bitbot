d8261bd47397198de44c2ba37489d1411250e144
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2014-02-03 17:09:01 +0100

    Create bbCmd.py

---
+++ source/bbCmd.py
@@ -0,0 +1,42 @@
+import sys
+import cmd
+sys.path.append('./source')
+sys.path.append('./api')
+
+from bbSettings import *
+from bbPerformance import *
+
+class bbCmd(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = 'B> '
+
+    
+    def do_performance(self, arg):
+        startDate = '2014-01-08'
+        endDate   = None
+        btflag    = 0
+        getTransactions(logFileName, transFileName)
+        if btflag is '1':
+            import backtest
+            logFileNameBT  = 'data/' + str(tradeBuffer)
+            logFileNameBT += str(priceWindow)
+            logFileNameBT += str(momFactor)
+            logFileNameBT += str(midDistance)
+            logFileNameBT += str(walkUp)
+            logFileNameBT += str(walkDown) + '.csv'
+            makePerformanceTable(logFileName, logFileNameBT, startDate,
+                                 endDate, transFileName)
+        else:
+            makePerformanceTable(logFileName, None, startDate, endDate,
+                                 transFileName)        
+    def help_performance(self):
+            print('Syntax: performance')
+            print('-- Calculates the performance between two dates for ' +
+                  'a client') 
+
+    def do_exit(self, arg):
+        sys.exit(1)
+    def help_exit(self):
+            print('Syntax: exit')
+            print('-- Terminates the application')

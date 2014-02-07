462ade23cf6bd250b2adb76d5a456828245216d8
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2014-02-07 17:05:14 +0100

    put returns in np.array

--- source/bbPerformance.py
+++ source/bbPerformance.py
@@ -34,7 +34,108 @@ def sharpe(r):
     avgReturn = r.mean()
     avgVol    = r.std()
     return avgReturn / avgVol * sqrt(365)
+    
+def totalReturn(r):
+    t = 1
+    for i in range(len(r)):
+        t *= (1 + r[i])
+    return (t-1)
+ 
+def calcReturn(log, transactions, date):
+    dft = log[np.datetime64(date + 'T00:01') : np.datetime64(date + 'T23:59')]
+    dfy = log[np.datetime64(date + 'T00:01') - np.timedelta64(1, 'D') : \
+             np.datetime64(date + 'T23:59') - np.timedelta64(1, 'D')]
+    try:
+        tdf = transactions[date]
+    except:
+        tdf = []
 
+    startValue = float(dfy.tail(1)['EUR'] + \
+                       dfy.tail(1)['BTC'] * dfy.tail(1)['Bid'])
+    endValue   = float(dft.tail(1)['EUR'] + \
+                       dft.tail(1)['BTC'] * dft.tail(1)['Bid'])
+    openPrice  = float((dfy.tail(1)['Bid'] + dfy.tail(1)['Ask'])/2)
+    closePrice = float((dft.tail(1)['Bid'] + dft.tail(1)['Ask'])/2)
+    
+    if len(tdf) != 0:
+        tdf['EUR']=tdf['Amount']*tdf['Bid']
+        endValue   = endValue - float(tdf.sum()['EUR'])
+    
+    retStrategy= endValue / startValue - 1
+    retHold    = closePrice / openPrice - 1
+    return openPrice, closePrice, retHold, retStrategy
+
+def getReturns(logFileName, transfers=None, start=None, end=None):
+    history   = pd.read_csv(logFileName, parse_dates=[0], index_col=0)
+ 
+    if start is None:
+        start = '2014-01-08'
+        npstart = np.datetime64(start + 'T23:50') - np.timedelta64(1, 'D')
+    else:
+        npstart = np.datetime64(start + 'T23:50') - np.timedelta64(1, 'D')
+    if end is None:
+        npend = None
+    else: 
+        npend = np.datetime64(end + 'T23:59')
+    history     = history[npstart:npend]
+ 
+    if transfers is None:
+        t = []
+    else:
+        t = pd.read_csv(transfers, parse_dates=[0], index_col=0)
+        t = t[npstart:npend]
+
+    dates       = history.index.values
+    uniqueDates = []
+    r = []
+    for d in range(len(dates)):
+        if str(dates[d])[0:10] not in uniqueDates:
+            uniqueDates.append(str(dates[d])[0:10])
+    for d in uniqueDates[1:]:
+            r.append(calcReturn(history, t, d))
+    return uniqueDates[1:], np.array(r)
+
+def printReturns(dates, returns):
+    sys.stdout.write('{0:<10}'.format('Date'))
+    sys.stdout.write('{0:>8}'.format('Open'))
+    sys.stdout.write('{0:>8}'.format('Close'))
+    sys.stdout.write('{0:>9}'.format('Buy&Hold'))
+    sys.stdout.write('{0:>10}'.format('Strategy'))
+    sys.stdout.write('\n')
+    for d in range(len(dates)):
+        sys.stdout.write('{0:<10}'.format(dates[d]))
+        sys.stdout.write('{0:>8.1f}'.format(returns[d][0]))
+        sys.stdout.write('{0:>8.1f}'.format(returns[d][1]))
+        sys.stdout.write('{0:>8.2f}'.format(100*returns[d][2]) + '%')
+        sys.stdout.write('{0:>8.2f}'.format(100*returns[d][3]) + '%')
+        sys.stdout.write('\n')
+
+def printSummary(returns):
+    sys.stdout.write('{0:<26}'.format('Total return:'))
+    sys.stdout.write('{0:>8.2f}'.format(100*totalReturn(returns[:,2])) + '%')    
+    sys.stdout.write('{0:>8.2f}'.format(100*totalReturn(returns[:,3])) + '%')
+    sys.stdout.write('\n')
+    
+    sys.stdout.write('{0:<26}'.format('Average return:'))
+    sys.stdout.write('{0:>8.2f}'.format(returns[:,2].mean()) + '%')    
+    sys.stdout.write('{0:>8.2f}'.format(returns[:,3].mean()) + '%')
+    sys.stdout.write('\n')
+    
+    sys.stdout.write('{0:<26}'.format('Sharpe ratio:'))
+    sys.stdout.write('{0:>8.2f}'.format(sharpe(returns[:,2])))    
+    sys.stdout.write('{0:>9.2f}'.format(sharpe(returns[:,3])))
+    sys.stdout.write('\n')
+    
+    sys.stdout.write('{0:<26}'.format('Sortino ratio:'))
+    sys.stdout.write('{0:>8.2f}'.format(sortino(returns[:,2])))    
+    sys.stdout.write('{0:>9.2f}'.format(sortino(returns[:,3])))
+    sys.stdout.write('\n')
+    
+    sys.stdout.write('{0:<26}'.format('Maximum drawdown:'))
+    sys.stdout.write('{0:>8.2f}'.format(100*drawdown(returns[:,2])) + '%')    
+    sys.stdout.write('{0:>8.2f}'.format(100*drawdown(returns[:,3])) + '%')
+    sys.stdout.write('\n')
+        
 def showPerformance(df, bt=None, tdf=[], date='Total'):
     if date != 'Total':
         dft = df[np.datetime64(date + 'T00:01') : np.datetime64(date + 'T23:59')]

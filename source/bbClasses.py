4de45360eb35c4c2bef837d16b3346b564b27eb3
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2014-02-10 13:41:48 +0100

    Update bbClasses.py

--- source/bbClasses.py
+++ source/bbClasses.py
@@ -47,8 +47,8 @@ class trader:
     def checkOverride(self, overrideFileName):
         with open(overrideFileName, 'rt') as of:
             data = of.readlines()
-        flagOverride = data(o[0].split('=')[-1].strip())
-        targetOverride = data(o[1].split('=')[-1].strip())
+        flagOverride = str(data[0].split('=')[-1].strip())
+        targetOverride = str(data[1].split('=')[-1].strip())
         if flagOverride is 1:
             self.target = targetOverride
 

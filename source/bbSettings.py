240ded1325f8350e12c5aaab788b5c5d50b64c05
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2014-02-17 14:19:49 +0100

    Update bbSettings.py

--- source/bbSettings.py
+++ source/bbSettings.py
@@ -1,21 +1,21 @@
-walkUp         = 0.15       # maxPrice = minPrice * (1 + walkUp): 0.15
-walkDown       = 0          # minPrice = maxPrice * (1 - walkDown): 0
-tradeFactor    = 0.00045    # minTrade = portfolioValue * tradeFactor: 0.00045
-momFactor      = 0.75       # target   = target * (1-Momentum*momFactor): 0.75
-priceWindow    = 100        # Window for weighted average
+WALKUP         = 0.15       # maxPrice = minPrice * (1 + walkUp): 0.15
+WALKDOWN       = 0          # minPrice = maxPrice * (1 - walkDown): 0
+TRADEFACTOR    = 0.00045    # minTrade = portfolioValue * tradeFactor: 0.00045
+MOMFACTOR      = 0.75       # target   = target * (1-Momentum*momFactor): 0.75
+PRICEWINDOW    = 100        # Window for weighted average
 
-backupFund     = 0.20        # % of funds reserved for cheap buys: NA
-allinLimit     = 0.10       # % of high for identifying cheap coins: NA
-stopLossLimit  = 0.20       # % of high for liquidating all coins: NA
+BACKUPFUND     = 0.20        # % of funds reserved for cheap buys: NA
+ALLINLIMIT     = 0.10       # % of high for identifying cheap coins: NA
+STOPLOSSLIMIT  = 0.20       # % of high for liquidating all coins: NA
  
-logFileName      = 'logs/history.csv'
-statusFileName   = 'logs/status.txt'
-plotFileName     = 'logs/plot.html'
-txFileName       = 'logs/transactions.csv'
+LOGFILENAME      = 'logs/history.csv'
+STATUSFILENAME   = 'logs/status.txt'
+PLOTFILENAME     = 'logs/plot.html'
+TXFILENAME       = 'logs/transactions.csv'
 
-transFileName    = 'data/funding.csv'
-logFileNameBT    = 'data/logBT.csv'
-freezeFileName   = 'data/freeze.txt'
-overrideFileName = 'data/override.txt'
+TRANSFILENAME    = 'data/funding.csv'
+LOGFILENAMEBT    = 'data/logBT.csv'
+FREEZEFILENAME   = 'data/freeze.txt'
+OVERRIDEFILENAME = 'data/override.txt'
 
-emailAddress     = 'philipp.g.deutsch@gmail.com'
+EMAILADDRESS     = 'philipp.g.deutsch@gmail.com'

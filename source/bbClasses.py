b054c678142bda3edd9cfa40cdfbfc4758b7293e
phildeutsch <philipp.g.deutsch@gmail.com>
2014-02-18 23:50:17 +0100

    indent in handle_error

--- source/bbClasses.py
+++ source/bbClasses.py
@@ -153,7 +153,7 @@ class trader:
     def handle_error(self, emailAddress, errorFileName):
         print(t.error)
         bbFunctions.sendEmail(self, m, emailAddress, self.error)
-            with open(errorFileName, 'at') as ef:
-                ef.write(m.time + '\t')
-                ef.write(self.error + '\n')
-            self.error = 0
+        with open(errorFileName, 'at') as ef:
+            ef.write(m.time + '\t')
+            ef.write(self.error + '\n')
+        self.error = 0

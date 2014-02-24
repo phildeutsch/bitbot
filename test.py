53a8396a18fcf03dbff40d71350c3b7123946712
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2014-02-24 14:57:46 +0100

    Update test.py

--- test.py
+++ test.py
@@ -9,3 +9,8 @@ from bbKeys import *
 
 public_client = apiBitstamp.Public()
 print(public_client.ticker()['volume'])
+trading_client = apiBitstamp.Trading(
+    username = '370147',
+    key = keyBitstamp,
+    secret = secBitstamp)
+print(trading_client.account_balance())
